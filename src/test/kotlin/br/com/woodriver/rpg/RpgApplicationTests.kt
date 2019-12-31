79defdeb77203042d9b580a36b9da58a1337870a
Yan Tapajós <ybrandao.d@gmail.com>
2019-12-31 11:16:37 -0300

    Added project to GitHub

---
+++ src/test/kotlin/br/com/woodriver/rpg/RpgApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.woodriver.rpg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RpgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
